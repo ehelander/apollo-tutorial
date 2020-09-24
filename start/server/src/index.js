acddcc0f4be8b3a564e1411fd3ae1b22454f5331
Eric Helander <eric.p.helander@gmail.com>
2020-09-24 13:46:30 -0500

    complete(build a schema)

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,1 +1,11 @@
-require('dotenv').config();
+require("dotenv").config();
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
+
+// Create an instance of ApolloServer and pass it the imported schema.
+const server = new ApolloServer({ typeDefs });
+
+// Log the URL.
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
