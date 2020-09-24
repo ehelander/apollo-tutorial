718d5e605ad066de25a8a4a4054d11943f0dd516
Eric Helander <eric.p.helander@gmail.com>
2020-09-24 16:31:05 -0500

    complete(connect tto data sources)

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,11 +1,21 @@
 require("dotenv").config();
 const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./schema");
+const { createStore } = require("./utils");
 
-// Create an instance of ApolloServer and pass it the imported schema.
-const server = new ApolloServer({ typeDefs });
+const LaunchAPI = require("./datasources/launch");
+const UserAPI = require("./datasources/user");
 
-// Log the URL.
+const store = createStore();
+
+const server = new ApolloServer({
+  typeDefs,
+  dataSources: () => ({
+    launchAPI: new LaunchAPI(),
+    userAPI: new UserAPI({ store }),
+  }),
+});
+
 server.listen().then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
 });
